a31dedbf8060810349bed387256b5acca388fa95
tapasb7 <tapasb7@yahoo.com>
2024-11-07 08:10:49 +0530

    first commit

---
+++ Nov/ex_05112024_Python_Basics/Lab002_Python_Basics.py
@@ -0,0 +1,5 @@
+print("hello World")
+print(2+2)
+print(2-2)
+print(3/3)
+print(2*2)
---
+++ Nov/ex_05112024_Python_Basics/Lab003_Python_Basics.py
@@ -0,0 +1,7 @@
+print("hello World")
+print("hello Tapas Banerjee")
+#def print(self, *args, sep=' ', end='\n', file=None):
+print ("Tapas",2345,"Properly written","separated by comma")
+print ("Tapas",2345,"Properly written","separated",sep='<>')
+print ("Tapas",2345,"Properly written","separated",sep='*',end='__')
+print ("Tapas",2345,"Properly written","separated by comma")
