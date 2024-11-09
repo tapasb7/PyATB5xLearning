48a1cd101e0527feaf75da48d893ffcb5522d250
tapasb7 <tapasb7@yahoo.com>
2024-11-09 09:31:59 +0530

    9th Nov task commits

---
+++ Nov/ex_09112024_Literals_Variables/lab007_task-1_add_sub_mul_div_3_decimal_numbers.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter the 1st number :"))
+num2 = int(input("Enter the 2nd number :"))
+num3 = int(input("Enter the 3rd number :"))
+
+sump = num1 + num2 + num3
+subs = num1 - num2 - num3
+mul = num1 * num2 * num3
+div = ((num1 / num2) / num3)
+
+print("addition of 3 numbers is :", sump)
+print("subtraction of 3 numbers is :", subs)
+print("multiplication of 3 numbers is :", mul)
+print("division result of ((a/b)/c) is ", div)
---
+++ Nov/ex_09112024_Literals_Variables/lab008_task-2_quotient_remainder_2_integers.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the 1st number :"))
+num2 = int(input("Enter the 2nd number :"))
+
+# num1 = 15
+# num2 = 2
+d = int((num1 / num2))
+c = num1 % num2
+print("Quotient is : ", d)
+print("Remainder is :" ,c)
