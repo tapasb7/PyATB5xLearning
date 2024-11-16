15ceab03dac274fdc0fd44db2fd2d6a7ae040bb3
tapasb7 <tapasb7@yahoo.com>
2024-11-16 11:25:17 +0530

    16 Nov task commit with Loop

---
+++ Nov/ex_16112024_Loops/lab021_while_loop.py
@@ -0,0 +1,4 @@
+i = 1
+while i < 5:
+    print("we are at below 5")
+    i = i + 1
---
+++ Nov/ex_16112024_Loops/lab022_16112024_task_FizzBuzz.py
@@ -0,0 +1,16 @@
+# Write a program that prints numbers from 1 to 100.
+# However, for multiples of 3, print "Fizz" instead of the number,
+# and for multiples of 5, print "Buzz.
+# " For numbers that are multiples of both 3 and 5, print "FizzBuzz." ( for, if)
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+        continue
+    elif i % 5 == 0:
+        print("Buzz")
+        continue
+    elif i % 3 == 0:
+        print("Fizz")
+        continue
+    print(i)
