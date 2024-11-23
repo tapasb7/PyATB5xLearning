9504f08564ca12cbbcfa43eee5814c406de10b39
tapasb7 <tapasb7@yahoo.com>
2024-11-23 06:22:22 +0530

    correct the logic in leapyear code

--- Nov/ex_14112024_Conditional staements/lab017_14112024_task_1_leapyear.py
+++ Nov/ex_14112024_Conditional staements/lab017_14112024_task_1_leapyear.py
@@ -14,7 +14,7 @@ year = int(input("Enter the Year :\n"))
 
 # # logic section include if,elif statement
 
-if (year % 400) == 0 and (year % 4) == 0 or (year % 100) != 0:
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(year, " is a Leap Year")
 else:
     print(year, " is not a Leap Year")
